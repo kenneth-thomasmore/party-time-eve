2d4f787b8dd7864a62bdcf5da36a69d2288c8901
kenneth-thomasmore <kenneth.andries@student.thomasmore.be>
2026-02-10 14:30:01 +0100

    created test list, links between pages and put bootstrap on it

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,18 +1,27 @@
 package be.thomasmore.party.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping({"/","/home"})
-    public String home(){
+    public String home(Model model){
+        int mycalculatedvalue = 34 * 62;
+        model.addAttribute("mycalculatedvalue", mycalculatedvalue);
     return "home";
     }
 
     @GetMapping("/about")
-    public String about(){
+    public String about(Model model){
+        String myname = "Evelyne";
+        String mystreet = "105 eepyville";
+        String mytown= "eepytown";
+        model.addAttribute("myname", myname);
+        model.addAttribute("mystreet", mystreet);
+        model.addAttribute("mytown", mytown);
         return "about";
     }
 }
